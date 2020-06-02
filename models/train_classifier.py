ebd7222b1f75a75b551366dcaf407d4673994d37
paonegri <paolonegrinotti@gmail.com>
2020-06-02 17:07:48 +0100

    feat: complete train_classifier script

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,122 @@
-import sys
+"""
+TRAIN CLASSIFIER
+Disaster Resoponse Project
+Udacity - Data Science Nanodegree
+How to run this script (Example)
+> python train_classifier.py ../data/disaster_dataset.db classifier.pkl
+Arguments:
+    1) SQLite db path (containing pre-processed data)
+    2) pickle file name to save ML model
+"""
 
+# import libraries
+import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report, f1_score, make_scorer
+from sklearn.decomposition import TruncatedSVD
+import pickle
+
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 
 def load_data(database_filepath):
-    pass
+    """
+    Load Data Function
+
+    input:
+        database_filepath: path to SQLite db
+    output:
+        X: feature DataFrame
+        Y: label DataFrame
+        category_names: used for data visualization (app)
+    """
+    # load data from database
+    engine = create_engine("sqlite:///content/""+database_filepath)
+    df = pd.read_sql_table("disaster_dataset", engine)
+    X = df['message']
+    Y = df.iloc[:,4:]
+    category_names = Y.columns
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    """
+    Tokenize function
+
+    input:
+        text: list of text messages (english)
+    output:
+        clean_tokens: tokenized text, clean for ML modeling
+    """
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
 
 
 def build_model():
-    pass
+    '''
+    Function to build a model, create pipeline, hypertuning as well as gridsearchcv
+
+    input:
+        N/A
+    output:
+        returns the model
+    '''
+    pipeline = Pipeline([
+       ('vect', CountVectorizer(tokenizer=tokenize)),
+       ('tfidf', TfidfTransformer()),
+       ('clf', MultiOutputClassifier(RandomForestClassifier()))])
+
+    parameters =  {
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [50, 100],
+        'clf__estimator__min_samples_split': [2, 4]
+        }
+
+    model = GridSearchCV(pipeline, param_grid=parameters)
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    Evaluate Model function
+
+    This function applies ML pipeline to a test set and prints out
+    model performance (accuracy and f1score)
+
+    input:
+        model: Scikit ML Pipeline
+        X_test: test features
+        Y_test: test labels
+        category_names: label names (multi-output)
+    """
+    y_pred = model.predict(X_test)
+    overall_accuracy = (y_pred == y_test).mean().mean()
+
+    print(classification_report(y_pred, Y_test.values, target_names=category_names))
+    print('overall accuracy {0:.4f} \n'.format(overall_accuracy))
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    Function to save the model
+    Input: model and the file path to save the model
+    Output: save the model as pickle file in the give filepath
+    '''
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
@@ -27,13 +125,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
